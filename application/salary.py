83c36554a8dd554df318d167276a2e12c32400a8
AlexBirtch <qBNXlrhVmimU9FDLXsT5>
2020-05-08 22:21:02 +1000

    Import. Module. Package

---
+++ application/salary.py
@@ -0,0 +1,6 @@
+from .db.people import get_employees
+
+def calculate_salary():
+    print(get_employees())
+
+    return calculate_salary()
---
+++ main.py
@@ -0,0 +1,12 @@
+from datetime import datetime, date, time
+from application.salary import calculate_salary
+
+def main():
+    time = datetime.now()
+    print(time)
+
+    print(calculate_salary())
+
+
+if __name__ == '__main__':
+    main()
